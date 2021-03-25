6f60fdd1787826be9cde69cb5d737fa899a0ff51
hungtm <manhhung23399@gmail.com>
2021-03-25 08:34:24 +0700

    Init package,base project apply MVP

---
+++ app/src/main/java/com/example/moviews/screen/search/SearchFragment.kt
@@ -0,0 +1,14 @@
+package com.example.moviews.screen.search
+
+import com.example.moviews.R
+import com.example.moviews.base.BaseFragment
+
+class SearchFragment : BaseFragment() {
+
+    override val layoutID: Int
+        get() = R.layout.fragment_search
+
+    override fun initComponents() {
+        TODO("Not yet implemented")
+    }
+}
